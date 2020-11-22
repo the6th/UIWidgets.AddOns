7f8eed0fa2a5119a77f8c66fff52fc3635f3cdac
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2020-11-22 13:20:13 +0900

    fix default colors

--- Assets/UIWidgets.AddOns/ColorPicker/Utils.cs
+++ Assets/UIWidgets.AddOns/ColorPicker/Utils.cs
@@ -61,23 +61,26 @@ namespace UIWidgets.AddOns
 
         public static List<Color> _defaultColors = new List<Color>
         {
-          Colors.deepPurple,
-          Colors.blue,
-          Colors.blue,
-          Colors.lightBlue,
-          Colors.cyan,
-          Colors.teal,
-          Colors.green,
-          Colors.lightGreen,
-          Colors.lime,
-          Colors.yellow,
-          Colors.amber,
-          Colors.orange,
-          Colors.deepOrange,
-          Colors.brown,
-          Colors.grey,
-          Colors.blueGrey,
-          Colors.black
+            Colors.red,
+            Colors.pink,
+            Colors.purple,
+            Colors.deepPurple,
+            Colors.indigo,
+            Colors.blue,
+            Colors.lightBlue,
+            Colors.cyan,
+            Colors.teal,
+            Colors.green,
+            Colors.lightGreen,
+            Colors.lime,
+            Colors.yellow,
+            Colors.amber,
+            Colors.orange,
+            Colors.deepOrange,
+            Colors.brown,
+            Colors.grey,
+            Colors.blueGrey,
+            Colors.black
         };
     }
 }
